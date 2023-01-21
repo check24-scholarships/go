4ae70099e48171106bde1852f1c6cac9c5cfae5c
Lantaan <andreas.iskrov@gmail.com>
2023-01-21 12:48:09 +0100

    cleaner code, error handling, time refresh on every request

--- HelloWorld.go
+++ HelloWorld.go
@@ -15,23 +15,27 @@ func main() {
 		Time string
 	}
 
-	indexBytes, err := os.ReadFile("./public/index.html")
-
-	publicData := PublicData{time.Now().Format("01-02-2006 15:04:05")}
-	tmpl, err := template.New("test").Parse(string(indexBytes))
-	if err != nil {
-		panic(err)
-	}
-
-	var tpl bytes.Buffer
-	if err = tmpl.Execute(&tpl, publicData); err != nil {
-		panic(err)
-	}
-
-	indexTemplated := tpl.String()
-
 	indexHandler := func(w http.ResponseWriter, req *http.Request) {
-		_, err := io.WriteString(w, indexTemplated)
+		indexBytes, err := os.ReadFile("./public/index.html")
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+		publicData := PublicData{time.Now().Format("01-02-2006 15:04:05")}
+
+		tmpl, err := template.New("test").Parse(string(indexBytes))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		var tpl bytes.Buffer
+		if err = tmpl.Execute(&tpl, publicData); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		indexTemplated := tpl.String()
+
+		_, err = io.WriteString(w, indexTemplated)
 		if err != nil {
 			return
 		}
