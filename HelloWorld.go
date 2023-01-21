146a4e9e704a1a617f92346fd4e1af6745088c1c
Lantaan <andreas.iskrov@gmail.com>
2023-01-21 12:15:35 +0100

    serve html (no templating)

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"html/template"
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// Hello world, the web server
-
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+	type PublicData struct {
+		Material string
+		Count    uint
+	}
+	indexBuffer, err := os.ReadFile("./public/index.html")
+	indexString := string(indexBuffer)
+	publicData := PublicData{"wool", 17}
+	tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(os.Stdout, publicData)
+	if err != nil {
+		panic(err)
 	}
 
-	http.HandleFunc("/", helloHandler)
+	indexHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, indexString)
+	}
+
+	http.HandleFunc("/", indexHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
